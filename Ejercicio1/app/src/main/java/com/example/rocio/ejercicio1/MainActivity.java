9e7f7649524f553f8a44d01df3c810dc2714e840
unknown <roguerrerogomez@gmail.com>
2017-11-11 08:42:21 -0600

    Se crea la base del proyecto login

---
+++ Ejercicio1/app/src/main/java/com/example/rocio/ejercicio1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.rocio.ejercicio1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    String user;
+    String password;
+    //Button boton=BtnBoton;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //BtnBoton.setOnClickListener(new OnClickListener() {
+            //@Override
+            //public void onClick(View arg0) {
+              //  Toast toast1 =
+                //        Toast.makeText(getApplicationContext(),
+                  //              "Toast por defecto", Toast.LENGTH_SHORT);
+
+//                toast1.show();
+  //          }
+    //    });
+    }
+}
